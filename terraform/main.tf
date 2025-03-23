0095addc2c8cd6ea3d3e6d5aa3e74dc14b83c620
Suyoung Kwon <youngandtom2@gmail.com>
2025-03-23 00:51:14 +0000

    new blank try

--- terraform/main.tf
+++ terraform/main.tf
@@ -202,7 +202,7 @@ resource "aws_s3_object" "obj_working" {
   bucket = data.aws_s3_bucket.soo_s3_bucket.id
   key    = "system-images/current-working.jpg"
   source = "system-images/current-working.jpg"
-  content_type = "image/jpg"
+  content_type = "image/jpeg"
   #etag = filemd5("path/to/file")
 }
 
@@ -210,7 +210,7 @@ resource "aws_s3_object" "obj_working_later" {
   bucket = data.aws_s3_bucket.soo_s3_bucket.id
   key    = "system-images/latter-working.jpg"
   source = "system-images/current-working.jpg"
-  content_type = "image/jpg"
+  content_type = "image/jpeg"
   #etag = filemd5("path/to/file")
 }
 
